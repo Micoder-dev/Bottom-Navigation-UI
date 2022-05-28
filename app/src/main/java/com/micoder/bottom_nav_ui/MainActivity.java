41c5968582432be28d3d5a84f391c42c45139ecf
Mugunthan <87799495+Micoder-dev@users.noreply.github.com>
2022-05-28 17:50:43 +0530

    bottom nav ui added

--- app/src/main/java/com/micoder/bottom_nav_ui/MainActivity.java
+++ app/src/main/java/com/micoder/bottom_nav_ui/MainActivity.java
@@ -3,12 +3,63 @@ package com.micoder.bottom_nav_ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int ID_HOME = 1;
+    private final int ID_MESSAGE = 2;
+    private final int ID_NOTIFICATION = 3;
+    private final int ID_ACCOUNT = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView selected_page=findViewById(R.id.selected_page);
+        MeowBottomNavigation bottomNavigation=findViewById(R.id.bottomNavigation);
+
+        bottomNavigation.add(new MeowBottomNavigation.Model(ID_HOME,R.drawable.ic_baseline_home_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(ID_MESSAGE,R.drawable.ic_baseline_message_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(ID_NOTIFICATION,R.drawable.ic_baseline_notifications_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(ID_ACCOUNT,R.drawable.ic_baseline_account_circle_24));
+
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+                Toast.makeText(MainActivity.this, "clicked item : "+ item.getId(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+
+                String name;
+                switch (item.getId()){
+                    case ID_HOME: name = "Home";
+                    break;
+
+                    case ID_MESSAGE: name = "Message";
+                    break;
+
+                    case ID_NOTIFICATION: name = "Notification";
+                    break;
+
+                    case ID_ACCOUNT: name = "Account";
+                    break;
+
+                    default: name = "";
+                }
+                selected_page.setText(getString(R.string.main_page_selected, name));
+            }
+        });
+
+        bottomNavigation.setCount(ID_NOTIFICATION, "4");
+        bottomNavigation.show(ID_HOME, true);
     }
 }
